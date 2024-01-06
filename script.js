0f2eb7bff67aec1c2481151b27e4ca4f237aefcd
zero12110 <155788708+zero12110@users.noreply.github.com>
2024-01-05 20:49:32 -0500

    Update script.js

--- script.js
+++ script.js
@@ -6,16 +6,16 @@ document.addEventListener('DOMContentLoaded', () => {
     let cardsWon = [];
 
     const cardArray = [
-        { name: 'card1', img: 'images/distracted.png' },
-        { name: 'card1', img: 'images/distracted.png' },
-        { name: 'card2', img: 'images/drake.png' },
-        { name: 'card2', img: 'images/drake.png' },
-        { name: 'card3', img: 'images/fine.png' },
-        { name: 'card3', img: 'images/fine.png' },
-        { name: 'card4', img: 'images/rollsafe.png' },
-        { name: 'card4', img: 'images/rollsafe.png' },
-        { name: 'card5', img: 'images/success.png' },
-        { name: 'card5', img: 'images/success.png' },
+        { name: 'card1', img: 'images/Cats.jpg' },
+        { name: 'card1', img: 'images/Cats.jpg' },
+        { name: 'card2', img: 'images/Meghan1.jpg' },
+        { name: 'card2', img: 'images/Meghan1.jpg' },
+        { name: 'card3', img: 'images/Meghan2.jpg' },
+        { name: 'card3', img: 'images/Meghan2.jpg' },
+        { name: 'card4', img: 'images/Meghan3.jpg' },
+        { name: 'card4', img: 'images/Meghan3.jpg' },
+        { name: 'card5', img: 'images/Meghangame.jpg' },
+        { name: 'card5', img: 'images/Meghangame.jpg' },
         // ...add more pairs as needed
     ];
 
